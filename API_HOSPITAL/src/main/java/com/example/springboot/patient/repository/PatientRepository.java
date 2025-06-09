b627dc08127e915a91e2cef166496c3fa7a0a90a
LucasFerrari14 <lucasferrarisoares@Gmail.com>
2025-06-09 15:26:34 -0300

    Finalizada a requisção
    Criada requisição que retorne uma lista de quartos que possua algum leito disponível: Especialidade/ala, código do quarto;

--- API_HOSPITAL/src/main/java/com/example/springboot/patient/repository/PatientRepository.java
+++ API_HOSPITAL/src/main/java/com/example/springboot/patient/repository/PatientRepository.java
@@ -1,6 +1,9 @@
 package com.example.springboot.patient.repository;
 
+import com.example.springboot.patient.projection.PatientHistoryProjection;
 import com.example.springboot.patient.model.PatientModel;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -8,4 +11,18 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PatientRepository extends JpaRepository<PatientModel, Long> {
+
+    @Query(nativeQuery = true,
+            value=
+                    "select" +
+                            "   HP.DT_DISCHARGE as dtDischarge, " +
+                            "   HP.de_specialty as deSpecialty, " +
+                            "   P.de_patient as ptName,  " +
+                            "   HP.dt_hospitalization as dtHospitalization " +
+                            "FROM" +
+                            "   CEH_HOSPITALIZATIONSLOG HP " +
+                            "JOIN CEH_PATIENT P ON P.CD_PATIENT = HP.CD_PATIENTE " +
+                            "WHERE" +
+                            "   HP.CD_PATIENTE = :cdPatient")
+    Page<PatientHistoryProjection> findHistoryHospitalization(@Param("cdPatient") Long cdPatient, Pageable pageable);
 }
--- API_HOSPITAL/src/main/java/com/example/springboot/patient/service/PatientService.java
+++ API_HOSPITAL/src/main/java/com/example/springboot/patient/service/PatientService.java
@@ -1,15 +1,18 @@
 package com.example.springboot.patient.service;
 
+import com.example.springboot.patient.projection.PatientHistoryProjection;
 import com.example.springboot.patient.DTO.PatientDTO;
+import com.example.springboot.patient.DTO.PatientHistoryDTO;
 import com.example.springboot.patient.model.PatientModel;
 import com.example.springboot.patient.repository.PatientRepository;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestBody;
-
+import org.springframework.data.domain.Pageable;
 import java.util.List;
 
 @Service
@@ -38,5 +41,15 @@ public class PatientService {
 
     public void delete(@NotNull PatientModel patient) {
         patientRepository.delete(patient);
+    }
+
+
+    public Page<PatientHistoryDTO> findHistoryHospitalization(Long cdPatient, Pageable pageable) {
+        Page<PatientHistoryProjection> page = patientRepository.findHistoryHospitalization(cdPatient, pageable);
+
+        return page.map(projection -> new PatientHistoryDTO(
+                projection.getPtName(), projection.getDeSpecialty(),
+                projection.getDtHospitalization(), projection.getDtDischarg()
+        ));
     }
 }
