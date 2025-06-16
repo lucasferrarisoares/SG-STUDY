946d109e0150caf957800435a77d19a932d7f1d8
LucasFerrari14 <lucasferrarisoares@Gmail.com>
2025-06-16 14:47:36 -0300

    base corrigida

--- API_HOSPITAL FRONTEND/dist/app.js
+++ API_HOSPITAL FRONTEND/dist/app.js
@@ -1,11 +1,11 @@
-"use strict";
-exports.__esModule = true;
 /// <reference types="angular-route" />
-var angular = require("angular");
-require("angular-route");
 var app = angular.module('meuApp', ['ngRoute']);
 app.config(['$routeProvider', function ($routeProvider) {
         $routeProvider
+            .when('/hospital', {
+            templateUrl: 'src/modules/Hospital/hospital.html',
+            controller: 'HospitaisController'
+        })
             .when('/home', {
             templateUrl: 'src/home/home.html',
             controller: 'MeuController'
@@ -13,4 +13,21 @@ app.config(['$routeProvider', function ($routeProvider) {
             .otherwise({
             redirectTo: '/home'
         });
+    }
+]);
+app.controller('MeuController', ['$scope', function ($scope) {
+        $scope.mensagem = 'Bem-vindo à Home!';
+    }]);
+app.controller('HospitaisController', ['$scope', function ($scope) {
+        $scope.hospitais = [];
+        $scope.novoHospital = '';
+        $scope.adicionarHospital = function () {
+            if ($scope.novoHospital) {
+                $scope.hospitais.push($scope.novoHospital);
+                $scope.novoHospital = '';
+            }
+        };
+        $scope.listarHospitais = function () {
+            // Aqui pode buscar de uma API, se quiser.
+        };
     }]);
--- API_HOSPITAL FRONTEND/src/app.ts
+++ API_HOSPITAL FRONTEND/src/app.ts
@@ -1,14 +1,16 @@
 /// <reference types="angular-route" />
-import * as angular from 'angular';
-import 'angular-route';
 
 const app = angular.module('meuApp', ['ngRoute']);
 
 app.config(['$routeProvider', function($routeProvider: ng.route.IRouteProvider) {
   $routeProvider
+    .when('/hospital', {
+      templateUrl: 'src/modules/Hospital/hospital.html',
+      controller: 'HospitaisController'
+    })
     .when('/home', {
-      templateUrl: 'src/home/home.html',
-      controller: 'MeuController'
+                templateUrl: 'src/home/home.html',
+                controller: 'MeuController'
     })
     .otherwise({
       redirectTo: '/home'
@@ -18,4 +20,20 @@ app.config(['$routeProvider', function($routeProvider: ng.route.IRouteProvider) 
 
 app.controller('MeuController', ['$scope', function($scope) {
   $scope.mensagem = 'Bem-vindo à Home!';
+}]);
+
+app.controller('HospitaisController', ['$scope', function($scope) {
+    $scope.hospitais = [];
+    $scope.novoHospital = '';
+
+    $scope.adicionarHospital = function() {
+        if ($scope.novoHospital) {
+            $scope.hospitais.push($scope.novoHospital);
+            $scope.novoHospital = '';
+        }
+    };
+
+    $scope.listarHospitais = function() {
+        // Aqui pode buscar de uma API, se quiser.
+    };
 }]);
