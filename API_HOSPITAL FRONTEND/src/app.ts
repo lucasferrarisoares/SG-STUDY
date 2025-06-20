fd9f34ae7fa5044558a77a8cbb3f2500a56a80a3
LucasFerrari14 <lucasferrarisoares@Gmail.com>
2025-06-20 17:46:28 -0300

    Organizando ambiente para segunda-feira

--- API_HOSPITAL FRONTEND/src/app.ts
+++ API_HOSPITAL FRONTEND/src/app.ts
@@ -3,26 +3,30 @@ import * as angular from 'angular';
 
 import HospitalListController from './modules/hospital/List/HospitalListController';
 import HospitalController from './modules/hospital/Edit/HospitalController';
-import PatientController from './modules/patient/PatientController';
+import PatientListController from './modules/patient/List/PatientListController';
 import hWingListController from './modules/hwing/List/hWingListController';
 import hWingController from './modules/hwing/Edit/hWingController';
-import BedController from './modules/bed/BedController';
-import RoomController from './modules/room/RoomController';
+import BedListController from './modules/bed/List/BedListController';
+import RoomListController from './modules/room/List/RoomListController';
 import HomeController from './home/HomeController';
 
 const app = angular.module('meuApp', ['ngRoute']);
 
 app.controller('HospitalController', HospitalController);
-app.controller('PatientController', PatientController);
-app.controller('hWingListController', hWingListController);
-app.controller('hWingController', hWingController); 
-app.controller('BedController', BedController);
-app.controller('RoomController', RoomController);
-app.controller('HomeController', HomeController);
 app.controller('HospitalListController', HospitalListController);
+app.controller('hWingController', hWingController); 
+app.controller('hWingListController', hWingListController);
+app.controller('PatientListController', PatientListController);
+app.controller('BedListController', BedListController);
+app.controller('RoomListController', RoomListController);
+app.controller('HomeController', HomeController);
 
 app.config(['$routeProvider', function($routeProvider: ng.route.IRouteProvider) {
   $routeProvider
+    .when('/home', {
+      templateUrl: 'src/home/Home.html',
+      controller: 'HomeController'
+    })
     .when('/hospital', {
       templateUrl: 'src/modules/hospital/List/HospitalList.html',
       controller: 'HospitalListController'
@@ -31,13 +35,9 @@ app.config(['$routeProvider', function($routeProvider: ng.route.IRouteProvider) 
       templateUrl: 'src/modules/hospital/Edit/Hospital.html',
       controller: 'HospitalController'
     })
-    .when('/home', {
-      templateUrl: 'src/home/Home.html',
-      controller: 'HomeController'
-    })
     .when('/patient', {
-      templateUrl: 'src/modules/patient/Patient.html',
-      controller: 'PatientController'
+      templateUrl: 'src/modules/patient/List/PatientList.html',
+      controller: 'PatientListController'
     })
     .when('/hwing', {
       templateUrl: 'src/modules/hwing/List/hWingList.html',
@@ -48,12 +48,16 @@ app.config(['$routeProvider', function($routeProvider: ng.route.IRouteProvider) 
       controller: 'hWingController'
     })
     .when('/bed', {
-      templateUrl: 'src/modules/bed/Bed.html',
-      controller: 'BedController'
+      templateUrl: 'src/modules/bed/List/BedList.html',
+      controller: 'BedListController'
     })
     .when('/room', {
-      templateUrl: 'src/modules/room/Room.html',
-      controller: 'RoomController'
+      templateUrl: 'src/modules/room/List/RoomList.html',
+      controller: 'RoomListController'
+    })
+    .when('/logs', {
+      templateUrl: 'src/modules/logs/List/LogsList.html',
+      controller: 'LogsListController'  
     })
     .otherwise({
       redirectTo: '/home'
